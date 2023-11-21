5ca1d233a4a157951cb032b30a91813653a99e91
Sedri14 <sedri14@gmail.com>
2023-11-21 12:11:10 +0200

    ISO code for languages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 const cheerio = require("cheerio");
 const { ZenRows } = require("zenrows");
+const ISO6391 = require('iso-639-1');
 require("dotenv").config();
 
 const JOKES_URL = "https://parade.com/968666/parade/chuck-norris-jokes/";
@@ -9,6 +10,7 @@ const TOKEN = process.env.TOKEN;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 let jokes = [];
+let userLanguage = 'en';
 
 const fetchJokes = async () => {
   const client = new ZenRows(process.env.ZENROWS_API_KEY);
@@ -30,16 +32,33 @@ const fetchJokes = async () => {
   }
 };
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-
+bot.onText(/set language (.+)/, (msg, match) => {
   try {
-    jokes = await fetchJokes();
+    const language = match[1].toLowerCase();
+    const isoCode = ISO6391.getCode(language);
+    if (isoCode) {
+      userLanguage = isoCode
+    } else {
+      console.log('Language not found.');
+      bot.sendMessage(msg.chat.id, 'Unsupported language. Please choose a valid language.');
+    }
   } catch (error) {
-    console.error("Error initializing jokes:", error.message);
+    console.error('Error:', error);
+    bot.sendMessage(msg.chat.id, 'An error occurred. Please try again.');
   }
-
-  const theJoke = jokes[1];
-  bot.sendMessage(chatId, theJoke);
-  jokes.length = 0;
 });
+
+// //this will be a bot.on of the joke number
+// bot.on("message", async (msg) => {
+//   const chatId = msg.chat.id;
+
+//   try {
+//     jokes = await fetchJokes();
+//   } catch (error) {
+//     console.error("Error initializing jokes:", error.message);
+//   }
+
+//   const theJoke = jokes[1];
+//   bot.sendMessage(chatId, theJoke);
+//   jokes.length = 0;
+// });
